8b158ffa6021947d952b6a82cd3347ccd41600ee
dav100ide <oreglio.dev@gmail.com>
2022-11-17 18:45:26 +0100

    origin

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const { createApp } = Vue;
+
+createApp({
+   data() {
+      return {
+         // data..
+         mmm: 'ciaoo',
+      };
+   },
+   methods: {
+      // metodi vue
+   },
+}).mount('#app');
